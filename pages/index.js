64d7e7a27443f5218f642bb847c00e2b98a8c41a
cyanblot <srijanjain1207@gmail.com>
2020-12-11 14:19:50 +0530

    auth

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
