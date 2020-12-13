ef805927fe99fac50d0f1a72694c1a819c243b6f
cyanblot <srijanjain1207@gmail.com>
2020-12-13 12:41:14 +0530

    UI  building

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,42 @@
-import { ThemeProvider } from '@emotion/react';
-import { AuthProvider } from '../lib/auth';
-import '../styles/globals.css';
-import theme from '../styles/theme';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+import { AuthProvider } from '@/lib/auth';
+import '@/styles/globals.css';
+import customTheme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
+    
+    <ChakraProvider theme={customTheme}>
       <AuthProvider>
+      <GlobalStyle/>
         <Component {...pageProps} />
       </AuthProvider>
-    </ThemeProvider>
+    </ChakraProvider>
   );
 };
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,42 @@
-import { useAuth } from '../lib/auth'
-
+import {
+  Button,
+  Code,
+  Heading,
+  Text,
+  Image,
+  Icon,
+  Box,
+  Flex
+} from '@chakra-ui/react';
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
-  const auth=useAuth();
+  const auth = useAuth();
 
   return (
     <div>
-    <main>
-    <h1> BeatMap</h1>
-    <p>
-      Current user: <code>{auth?.user?.email}</code>
-    </p>
-        <button onClick={(e)=> auth.signinWithGitHub()}>Sign In</button>
-        
-        {auth?.user && (<button onClick={(e)=> auth.signout()}>Sign Out</button>)}
-      </main>
-
-
+      <head>
+        <title>BeatMap</title>
+      </head>
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        maxW="300px"
+      >
+        <Image boxSize="sm" src="BeatMap.png" alt="BeatMap logo" />
+        <Heading> BeatMap</Heading>
+        {/* <Icon color="black" name="logo" size="100px"></Icon> */}
+        <Text>
+          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+        </Text>
+        {auth.user ? (
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
+        )}
+      </Flex>
     </div>
-  )
+  );
 }
