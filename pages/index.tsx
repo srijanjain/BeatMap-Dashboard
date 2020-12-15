405552d2d66b9ef5354ea4560faa8acf788174ce
cyanblot <srijanjain1207@gmail.com>
2020-12-16 00:41:23 +0530

    video added

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,14 +82,12 @@ const Index = () => {
               
                 <Flex flexDirection="column" p={8}>
                   <AspectRatio w="full" mb={2}>
-                    <iframe
-                      width="560"
-                      height="315"
-                      src="https://www.youtube.com/embed/f49FlCwZRXU"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen
-                    ></iframe>
+                  <iframe
+    title="naruto"
+    src="https://www.youtube.com/embed/f49FlCwZRXU"
+    allowFullScreen
+  />
+
                   </AspectRatio>
 
                   <Flex>
